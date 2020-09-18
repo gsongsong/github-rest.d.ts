1d28d4df28f02f0c046fc80971f6b32a04f4ff27
Seokseong Jeon <sjeon87@gmail.com>
2020-09-18 19:41:29 +0900

    Initial commit. Add repos.content.ContentDirectory[Item]

---
+++ README.md
@@ -0,0 +1,9 @@
+# github-rest.d.ts
+
+TypeScript declarations for GitHub REST API.
+Not all APIs are covered.
+Updates will be done on demand.
+
+## Reference
+
+- https://docs.github.com/en/rest
---
+++ src/repos/content.ts
@@ -0,0 +1,26 @@
+/**
+ * Response if content is a directory
+ * https://docs.github.com/en/rest/reference/repos#get-repository-content
+ */
+export type ContentDirectory = ContentDirectoryItem[];
+
+/**
+ * Individual item of {@link ContentDirectory}
+ * https://docs.github.com/en/rest/reference/repos#get-repository-content
+ */
+export type ContentDirectoryItem = {
+  "type": string;
+  "size": number;
+  "name": string;
+  "path": string;
+  "sha": string;
+  "url": string;
+  "git_url": string;
+  "html_url": string;
+  "download_url": string;
+  "_links": {
+    "self": string;
+    "git": string;
+    "html": string;
+  };
+}
