a0ac701c4b5b1960e8ce3fd2538fcca8922761bc
Seokseong Jeon <sjeon87@gmail.com>
2020-09-20 19:19:09 +0900

    Add rateLimit.RateLimit

---
+++ dist/rateLimit.d.ts
@@ -0,0 +1,32 @@
+/**
+ * https://docs.github.com/en/rest/reference/rate-limit
+ */
+export declare type RateLimit = {
+    "resources": {
+        "core": {
+            "limit": number;
+            "remaining": number;
+            "reset": number;
+        };
+        "search": {
+            "limit": number;
+            "remaining": number;
+            "reset": number;
+        };
+        "graphql": {
+            "limit": number;
+            "remaining": number;
+            "reset": number;
+        };
+        "integration_manifest": {
+            "limit": number;
+            "remaining": number;
+            "reset": number;
+        };
+    };
+    "rate": {
+        "limit": number;
+        "remaining": number;
+        "reset": number;
+    };
+};
